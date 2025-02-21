64ab67a1ec1c5dcfb7bf0068e19f2bf29afd1a47
Arturo Orient Romero <arturorient1305@gmail.com>
2025-02-21 10:24:52 +0100

    Terminamos la conf de register el cual sube los datos a mongo y por ultimo envia un email de saludo al usuario

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from smtp_utils import saludo_email
 
 app = Flask("__name__")
 app.secret_key = "f18440b8772ce1f74c8877a8616dc190624015e870e9ac8d868c9b42b7027a7b"
@@ -38,22 +39,40 @@ def load_user(user_id):
 def home():
     return render_template("home.html")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form.get["username"]
-        email = request.form.get["email"]
-        password = request.form.get["password"]
+        try:
+            username = request.form.get["username"]
+            email = request.form.get["email"]
+            password = request.form.get["password"]
+            if not username or not email or not password:
+                print("Por favor rellena todos los campos")
+                return redirect(url_for("login"))
+            elif password.leng() <= 6 and password.leng() >= 12:
+                print("Por favor ingrese una contraseña con un valor entre 6 y 12 caracteres")
+                return redirect(url_for("login"))
+            elif password.include(" "):
+                print("La contraseña no puede tener espacion en blanco")
+                return redirect(url_for("login"))
+            elif password == "admin":
+                print("No puedes usar la palabra admin")
+            elif usuarios.find_one({"username" : username}) or usuarios.find_one({"email" : email}):
+                print("El usuario ya existe por favor inicia sesión")
+                return redirect(url_for("login"))
+            new_user_set = usuarios.insert_one({"username" : username, "email":email, "password": generate_password_hash(password)}).inserted_id
+            new_user = User(new_user_set["_id"], new_user_set["username"], new_user_set["email"])
+            load_user(new_user)
+            print("Usuario agregado con exito")
+            saludo_email(email, username)
+            print("Correo enviado con exito")
+            return redirect(url_for("perfil")) 
+        except Exception as e:
+            print (e)
 
-        if not username or not email or not password:
-            print("Por favor rellena todos los campos")
-            return redirect(url_for("login"))
-        elif password.leng() <= 6 and password.leng() >= 12:
-            print("Por favor ingrese una contraseña con un valor entre 6 y 12 caracteres")
-            return redirect(url_for("login"))
-        elif password.include(" "):
-            print("La contraseña no puede tener espacion en blanco")
-            return redirect(url_for("login"))
+    return render_template("register.html")
+
+        
         
         
 
